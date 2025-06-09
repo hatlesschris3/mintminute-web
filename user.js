75554e571d057bdf81147a91f28183d40e15b5b2
hatlesschris3 <hatlesschris@gmail.com>
2025-06-09 14:57:09 +0100

    Update user.js

--- user.js
+++ user.js
@@ -3,7 +3,8 @@ const defaultUser = {
   age: 13,
   balance: 0.00,
   prefKey: "TCH3-GME2-TY1",
-  history: []
+  history: [],
+  adPref: "any"
 };
 
 function loadUser() {
@@ -27,22 +28,6 @@ function updateBalance(amount) {
   saveUser(user);
 }
 
-function getUserBalance() {
-  const user = loadUser();
-  return user ? user.balance.toFixed(2) : "0.00";
-}
-
 function resetUser() {
   localStorage.removeItem("mintUser");
-}
-
-function getPrefKey() {
-  const user = loadUser();
-  return user ? user.prefKey : "Unknown";
-}
-
-function setPrefKey(newKey) {
-  const user = loadUser();
-  user.prefKey = newKey;
-  saveUser(user);
 }
